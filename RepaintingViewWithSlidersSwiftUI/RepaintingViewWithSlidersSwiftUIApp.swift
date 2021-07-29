99f35568040799072366d63981408198f6f6dace
Ios Kateyka <ioskateyka@gmail.com>
2021-07-29 22:29:07 +0300

    Initial Commit

---
+++ RepaintingViewWithSlidersSwiftUI/RepaintingViewWithSlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepaintingViewWithSlidersSwiftUIApp.swift
+//  RepaintingViewWithSlidersSwiftUI
+//
+//  Created by Екатерина Боровкова on 29.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RepaintingViewWithSlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
